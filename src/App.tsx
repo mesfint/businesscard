51076275b74b863b67ca205f2f0c8cb5e464f5a6
mesfint <mesfin_meng@yahoo.com>
2023-11-20 20:23:31 +0200

    UI container

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,22 @@ function App() {
   return (
     <>
       <Inspx>
-        <Text variant="heading-one">Business Card</Text>
+        <div className="bg-light-green flex items-center justify-center py-[145px] h-[982px] pb-[195px]">
+          <div className=" bg-white   mx-[195px] h-[667px]  rounded-[10px] px-[40px] py-[40px] shadow-lg">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+            quisquam praesentium accusamus nam eveniet necessitatibus error
+            cupiditate veniam minima numquam, nulla recusandae ducimus sequi
+            aperiam consectetur ad omnis sunt! Natus? Delectus, perspiciatis
+            aperiam? Repellendus id ullam at nihil expedita, quas consequatur
+            necessitatibus sint ab iure dolor ut laudantium quasi impedit,
+            sapiente repellat illum, blanditiis totam maiores quisquam minus!
+            Veritatis, sapiente? Totam sequi perferendis inventore blanditiis
+            nostrum sunt voluptatum ullam delectus omnis ab saepe voluptatem
+            possimus, accusantium, tempore maxime iure alias quo quasi corporis
+            placeat. Deleniti labore rem incidunt perferendis odio? Aut
+            aspernatur voluptatibus nisi itaque dolor eveniet laboriosam culpa
+          </div>
+        </div>
       </Inspx>
     </>
   );
